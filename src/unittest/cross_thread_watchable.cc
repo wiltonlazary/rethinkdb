2308f3f12a96684185c33ec94d43fdab2e326289
Daniel Mewes <daniel@rethinkdb.com>
2015-09-01 14:08:20 -0700

    Avoid most-vexing-parse issue with threadnum_t in unit tests.

--- src/unittest/cross_thread_watchable.cc
+++ src/unittest/cross_thread_watchable.cc
@@ -18,7 +18,7 @@ TPTEST(CrossThreadWatchable, CrossThreadWatchableTest) {
     scoped_ptr_t<watchable_variable_t<int> > watchable;
     scoped_ptr_t<cross_thread_watchable_variable_t<int> > ctw;
     {
-        on_thread_t thread_switcher(threadnum_t(0));
+        on_thread_t thread_switcher((threadnum_t(0)));
         watchable.init(new watchable_variable_t<int>(0));
 
         ctw.init(new cross_thread_watchable_variable_t<int>(watchable->get_watchable(), threadnum_t(1)));
@@ -29,12 +29,12 @@ TPTEST(CrossThreadWatchable, CrossThreadWatchableTest) {
     try {
         for (i = 1; i < 100; ++i) {
             {
-                on_thread_t switcher(threadnum_t(0));
+                on_thread_t switcher((threadnum_t(0)));
                 watchable->set_value(i);
                 expected_value = i;
             }
             {
-                on_thread_t switcher(threadnum_t(1));
+                on_thread_t switcher((threadnum_t(1)));
                 signal_timer_t timer;
                 timer.start(5000);
                 ctw->get_watchable()->run_until_satisfied(
@@ -44,7 +44,7 @@ TPTEST(CrossThreadWatchable, CrossThreadWatchableTest) {
 
         for (; i < 1000; ++i) {
             {
-                on_thread_t switcher(threadnum_t(0));
+                on_thread_t switcher((threadnum_t(0)));
                 int step = i + 25;
                 for (; i < step; ++i) {
                     watchable->set_value(i);
@@ -52,7 +52,7 @@ TPTEST(CrossThreadWatchable, CrossThreadWatchableTest) {
                 }
             }
             {
-                on_thread_t switcher(threadnum_t(1));
+                on_thread_t switcher((threadnum_t(1)));
                 signal_timer_t timer;
                 timer.start(5000);
                 ctw->get_watchable()->run_until_satisfied(
