072432bb620f92afeaf5d46f4b2f000713e1cff4
Daniel Mewes <daniel@rethinkdb.com>
2015-09-03 14:24:35 -0700

    Put a static_cast in a different place

--- src/rdb_protocol/query_cache.cc
+++ src/rdb_protocol/query_cache.cc
@@ -343,13 +343,13 @@ void query_cache_t::ref_t::serve(env_t *env, Response *res) {
     // The rationale behind limiting the number of threads to 16 is that if we
     //   use a lot of threads, the risk increases that one of them will be under
     //   higher-than-average load, which in turn would lead to a high latency for us.
-    int num_threads = std::min(16, get_num_db_threads());
+    int64_t num_threads = std::min<int64_t>(16, get_num_db_threads());
     static const size_t PARALLELIZATION_THRESHOLD = 500;
     if (ds.size() <= PARALLELIZATION_THRESHOLD) {
         num_threads = 1;
     }
     int32_t thread_offset = get_thread_id().threadnum;
-    pmap(static_cast<int64_t>(num_threads), [&] (int64_t m) {
+    pmap(num_threads, [&] (int64_t m) {
         int32_t my_thread =
             (thread_offset + static_cast<int32_t>(m)) % get_num_db_threads();
         on_thread_t switcher((threadnum_t(my_thread)));
