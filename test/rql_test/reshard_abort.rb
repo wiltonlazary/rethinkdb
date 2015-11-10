19d2456271e893bf8e36ee5b8ba0db2020bd226a
Michael Lucy <mlucy@rethinkdb.com>
2015-11-09 22:31:24 -0800

    Improved resharding abortion test.

--- test/rql_test/reshard_abort.rb
+++ test/rql_test/reshard_abort.rb
@@ -22,6 +22,8 @@ r.table('test').insert((0...1000).map{|i| {id: i, a: i}}).run
 
 [{max_batch_rows: 1}, {max_batch_rows: 10}].each {|bo|
   [lambda {|x| x},
+   lambda {|x| x.get_all(r.args(r.range(100).coerce_to('array')))},
+   lambda {|x| x.get_all(r.args(r.range(100).coerce_to('array')), index: 'a')},
    lambda {|x| x.orderby(index: 'id')},
    lambda {|x| x.orderby(index: r.desc('id'))},
    lambda {|x| x.orderby(index: 'a')},
@@ -36,8 +38,41 @@ r.table('test').insert((0...1000).map{|i| {id: i, a: i}}).run
     y = nil
     begin
       y = stream.to_a.size
-    rescue Exception => e
+    rescue RethinkDB::RqlError => e
       puts "Stream successfully aborted: #{e}."
+    end
+    raise "Stream failed to abort (got #{y})!" if y
+  }
+}
+
+[{max_batch_rows: 1}, {max_batch_rows: 10}].each {|bo|
+  [lambda {|x| x},
+   lambda {|x| x.get_all(r.args(r.range(100).coerce_to('array')))},
+   lambda {|x| x.get_all(r.args(r.range(100).coerce_to('array')), index: 'a')},
+   lambda {|x| x.between(100, 800, index: 'id')},
+   lambda {|x| x.between(100, 800, index: 'a')}].each {|func|
+    op = func.call(r.table('test'))
+    puts "Testing #{op.inspect}.run(#{bo})..."
+    stream1 = op.changes(include_initial: true).run(bo)
+    x = stream1.next
+    stream2 = op.changes().run(bo)
+    r.table('test').reconfigure(shards: n_shards(), replicas: 1).run
+    r.table('test').wait.run
+    y = nil
+    begin
+      puts "Testing #{op.inspect}.changes(include_initial: true).run(#{bo})..."
+      loop { y = stream1.next(3) }
+
+    rescue RethinkDB::RqlError => e
+      puts "Stream successfully aborted: #{e}."
+      y = nil
+      begin
+        puts "Testing #{op.inspect}.changes().run(#{bo})..."
+        loop { y = stream2.next(3) }
+      rescue RethinkDB::RqlError => e
+        puts "Stream successfully aborted: #{e}."
+        y = nil
+      end
     end
     raise "Stream failed to abort (got #{y})!" if y
   }
