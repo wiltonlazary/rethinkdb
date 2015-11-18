29117cd365f1e741451e2e84e5a257ff6da31109
Karl Kuehn <kuehn.karl@gmail.com>
2015-11-17 16:07:32 -0800

    reverting the version info

--- drivers/python/setup.py
+++ drivers/python/setup.py
@@ -1,23 +1,30 @@
 # Copyright 2010-2015 RethinkDB, all rights reserved.
 
-import setuptools
-import rethinkdb
+import os, setuptools, sys
+
+modulePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rethinkdb')
+sys.path.insert(0, modulePath)
+from version import version
+sys.path.remove(modulePath)
 
 conditionalPackages = []
-try:
-    import asyncio
-    conditionalPackages = ['rethinkdb.asyncio']
-except ImportError: pass
+if 'upload' in sys.argv: # ensure that uplodas always have everything while uploading to pypi
+    conditionalPackages = ['rethinkdb.asyncio_net']
+else:
+    try: # only add asyncio when it is supported per #4702
+        import asyncio
+        conditionalPackages = ['rethinkdb.asyncio_net']
+    except ImportError: pass
 
 setuptools.setup(
     name="rethinkdb",
     zip_safe=True,
-    version=rethinkdb.__version__,
+    version=version,
     description="Python driver library for the RethinkDB database server.",
     url="http://rethinkdb.com",
     maintainer="RethinkDB Inc.",
     maintainer_email="bugs@rethinkdb.com",
-    packages=['rethinkdb', 'rethinkdb.tornado', 'rethinkdb.twisted', 'rethinkdb.backports.ssl_match_hostname'] + conditionalPackages,
+    packages=['rethinkdb', 'rethinkdb.tornado_net', 'rethinkdb.twisted_net', 'rethinkdb.backports.ssl_match_hostname'] + conditionalPackages,
     package_dir={'rethinkdb':'rethinkdb'},
     package_data={ 'rethinkdb':['backports/ssl_match_hostname/*.txt'] },
     entry_points={
