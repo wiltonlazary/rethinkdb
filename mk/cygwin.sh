02af02b388b2d805301f3467edf1517cabb7de04
Etienne Laurin <atnnn@rethinkdb.com>
2016-01-20 12:39:39 -0800

    32-bit build

--- mk/cygwin.sh
+++ mk/cygwin.sh
@@ -12,11 +12,11 @@ format_output () {
       s|([a-zA-Z]):\\|/cygdrive/\1/|g;          # convert to cygwin path
       s|\\|/|g;                                 # mirror slashes
       s|( [^ ]+\.cc){10,}| ...|;                # do not list all cc files on a single line
-      print unless m{^         x64/.*\.obj.?$}' # do not list all object files when linking
+      print unless m{^         .*/[0-9]/.*\.obj.?$}' # do not list all object files when linking
 }
 
 if [[ ! -e RethinkDB.vcxproj ]]; then
     mk/gen-vs-project.sh
 fi
 
-"$MSBUILD" /p:Configuration=Release /p:PlatformToolset=v140 /property:Platform=x64 /maxcpucount "$@" RethinkDB.vcxproj | format_output
+"$MSBUILD" /p:Configuration=Debug /p:PlatformToolset=v140 /property:Platform=Win32 /maxcpucount "$@" RethinkDB.vcxproj | format_output
